c3fe956d87d479fb69c49424abd3f2b077a4ef0f
Juwen Nagpal <Juwennagpal6@gmail.com>
2023-02-28 19:02:05 +0100

    Updated

--- setup.py
+++ setup.py
@@ -27,8 +27,11 @@ setup(
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    options={
+        'bdist_wheel': {
+            'universal': True
+        }
+    }
 )
